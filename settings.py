04870024af524bff195e96bb4bce00b71f697075
el-fantasma <62445971+el-fantasma@users.noreply.github.com>
2020-03-25 03:01:21 +0700

    Удалён API_KEY

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-TG_TOKEN = "1075912832:AAF1n7hjFG4h7i1PFMhZUhSBTccvfXZCNTI"
+TG_TOKEN = ""
 TG_API_URL = "https://telegg.ru/orig/bot"
 
 USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':dog:',]
